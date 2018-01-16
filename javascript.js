234ab50c15b4a13b5bf54dc343d895bcf14dfc5d
trodrigs1120 <trodrigs@yahoo.com>
2018-01-16 15:37:31 -0500

    3 search functionalities online

--- javascript.js
+++ javascript.js
@@ -12,33 +12,36 @@ $(document).ready(function () {
     })
 // We're pulling the ISBN, book title, author, average rating? from google books
 $("#Click").on("click", function(){
+    $(".PageBody").empty();
+    GoogleBooksAPI()
 
-if($('#Click').hasClass('BAuthor')) {
-    
-}
-if($('#Click').hasClass('BTitle')) {
-    GoogleBooksTitle();
-    
-}
-if($('#Click').hasClass('BISBN')) {
-    GoogleBooksISBN()
-}
 
 })
-function GoogleBooksTitle(){
+function GoogleBooksAPI(){
+    var SearchVariable = $("#SearchTerm").val();
+    queryURL = ""
+    if($('#Click').hasClass('BAuthor')) {
+        queryURL = "https://www.googleapis.com/books/v1/volumes?q=inauthor:"+SearchVariable
+    }
+    if($('#Click').hasClass('BTitle')) {
+        queryURL = "https://www.googleapis.com/books/v1/volumes?q="+SearchVariable  
+    }
+    if($('#Click').hasClass('BISBN')) {
+        queryURL = "https://www.googleapis.com/books/v1/volumes?q=isbn:"+SearchVariable
+    }
     var SearchVariable = $("#SearchTerm").val();
     console.log(SearchVariable)
-    queryURL = "https://www.googleapis.com/books/v1/volumes?q="+SearchVariable
     console.log(queryURL)
     $.ajax({
         url: queryURL,
         method: "GET"
         }).done(function(response) {
-            var ISBN13=response.items[0].volumeInfo.industryIdentifiers[0].identifier;
-            var ISBN10=response.items[0].volumeInfo.industryIdentifiers[1].identifier;
-            var Author=response.items[0].volumeInfo.authors[0]
-            var Title=response.items[0].volumeInfo.title
-            var Image=response.items[0].volumeInfo.imageLinks.thumbnail
+            for (var i = 0; i<5; i++ ){
+            var ISBN13=response.items[i].volumeInfo.industryIdentifiers[0].identifier;
+            var ISBN10=response.items[i].volumeInfo.industryIdentifiers[1].identifier;
+            var Author=response.items[i].volumeInfo.authors[0]
+            var Title=response.items[i].volumeInfo.title
+            var Image=response.items[i].volumeInfo.imageLinks.thumbnail
             // next to it in the array are bigger images for the full book page
             // In case we need either ISBN
             console.log ("ISBN 13 and 10")
@@ -47,31 +50,13 @@ function GoogleBooksTitle(){
             console.log(Author)
             console.log(response.items[0].volumeInfo.imageLinks.thumbnail)
            console.log(Title)
+           $(".PageBody").append('<p> Title: '+Title+'</p>')
+           $(".PageBody").append('<p> Author: '+Author)
+           $(".PageBody").append('<p> ISBN: '+ISBN10)
+           $(".PageBody").append('<img id="theImg" src='+Image+' />')
+           $(".PageBody").append('<br>')
+        }
 });
-}
-function GoogleBooksISBN(){
-    var SearchVariable = $("#SearchTerm").val();
-    console.log(SearchVariable)
-    queryURL = "https://www.googleapis.com/books/v1/volumes?q=isbn:0060007761"
-    console.log(queryURL)
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).done(function(response) {
-            var ISBN13=response.items[0].volumeInfo.industryIdentifiers[0].identifier;
-            var ISBN10=response.items[0].volumeInfo.industryIdentifiers[1].identifier;
-            var Author=response.items[0].volumeInfo.authors[0]
-            var Title=response.items[0].volumeInfo.title
-            var Image=response.items[0].volumeInfo.imageLinks.thumbnail
-            // next to it in the array are bigger images for the full book page
-            // In case we need either ISBN
-            console.log ("ISBN 13 and 10")
-            console.log (ISBN13)  
-            console.log (ISBN10)
-            console.log(Author)
-            console.log(response.items[0].volumeInfo.imageLinks.thumbnail)
-           console.log(Title)
-                                    })
 }
 
 
