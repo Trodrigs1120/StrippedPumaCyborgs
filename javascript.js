32214b65ee6f026b5a39e99f8b0d99992bd0e0ff
trodrigs1120 <trodrigs@yahoo.com>
2018-01-18 14:44:45 -0500

    added full page view for a given item

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var SearchableTopics =["BAuthor", "BTitle","BISBN" , "MArtist" , "MAlbum" , "MTrack"];
-var Button;
-var IsEmpty = true;
+//var Button;
+var ItemValue="";
 var APIKey = ""
 
 
@@ -42,43 +42,94 @@ function GoogleBooksAPI(){
             var Author=response.items[i].volumeInfo.authors[0]
             var Title=response.items[i].volumeInfo.title
             var Image=response.items[i].volumeInfo.imageLinks.thumbnail
+            var Rating=response.items[i].volumeInfo.averageRating
+            console.log(Rating)
             // next to it in the array are bigger images for the full book page
             // In case we need either ISBN
-            console.log ("ISBN 13 and 10")
-            console.log (ISBN13)  
-            console.log (ISBN10)
-            console.log(Author)
-            console.log(response.items[0].volumeInfo.imageLinks.thumbnail)
-           console.log(Title)
-           $(".PageBody").append('<p> Title: '+Title+'</p>')
-           $(".PageBody").append('<p> Author: '+Author)
-           $(".PageBody").append('<p> ISBN: '+ISBN10)
-           $(".PageBody").append('<img id="theImg" src='+Image+' />')
-           $(".PageBody").append('<br>')
+        //    console.log ("ISBN 13 and 10")
+        //    console.log (ISBN13)  
+        //    console.log (ISBN10)
+        //    console.log(Author)
+        //    console.log(response.items[0].volumeInfo.imageLinks.thumbnail)
+        //   console.log(Title)
+
+           
+           var btn = document.createElement('button');
+           var wrapper = document.createElement('div');
+           wrapper.appendChild(btn);
+           btn.classList.add("Click");
+           btn.classList.add("Button"+i);
+           btn.setAttribute("ArraySlot", i );
+           $(".PageBody").append(wrapper)
+           var buttons = wrapper.getElementsByTagName("BUTTON")
+           buttons[0].onclick = function(){ 
+            ItemValue = ($(this).attr("arrayslot"));
+            // Takes the array slot of the book. We can rerun the query for only that array slot
+             LargeView()
+            }     
+
+            $(".Button"+i).append('<p> Title: '+Title+'</p>')
+           // Commented out for now. need a better button/href for the link to the "whole page"
+           //$(".PageBody").append('<p> Title: '+Title+'</p>')
+            $(".PageBody").append('<p> Author: '+Author)
+            $(".PageBody").append('<p> ISBN: '+ISBN10)
+           
+           if (Rating!=undefined){
+            $(".PageBody").append('<p> Average Rating: '+Rating+' stars</p>')
+         }
+            $(".PageBody").append('<img id="theImg" src='+Image+' />')
+            $(".PageBody").append('<br>')
+      
+           // We need a button that gets appended that is clickable to take you to a new page that allows us to 
+           // do the actual heres the book and its reviews get hype
         }
 });
 }
+// going to try to imitate bootstrap divs and classes to see how it all goes
+function LargeView(){
+    $(".PageBody").empty()
+   $(".PageBody").append($('<div />', {
+    class: 'col-md-6',
+    id: 'Art'
+    }));
+    $(".PageBody").append($('<div />', {
+        class: 'col-md-6',
+        id: 'Content'
+        }));
+    $(".PageBody").append($('<div />', {
+        class: 'col-md-6',
+        id: 'Reviews'
+    }));        
+   
 
-
-// Unused for now 
-function GetBookData(){
+   console.log(ItemValue)
+// if we run into search issues we can change the query to search by ISBN which should give us a 99% chance of getting the right book
+   $.ajax({
+    url: queryURL,
+    method: "GET"
+    }).done(function(response) {
+        var ISBN13=response.items[ItemValue].volumeInfo.industryIdentifiers[0].identifier;
+        var ISBN10=response.items[ItemValue].volumeInfo.industryIdentifiers[1].identifier;
+        var Author=response.items[ItemValue].volumeInfo.authors[0]
+        var Title=response.items[ItemValue].volumeInfo.title
+        var Image=response.items[ItemValue].volumeInfo.imageLinks.thumbnail
+        var Description=response.items[ItemValue].volumeInfo.description
+        var Rating=response.items[ItemValue].volumeInfo.averageRating
+        var BuyLink=response.items[ItemValue].saleInfo.buyLink
+        var IsFiction=response.items[ItemValue].volumeInfo.categories[0]
+        console.log(Rating + BuyLink)
+        console.log(IsFiction)
+        $("#Art").append('<img id="theImg" src='+Image+' />');
+        // art image is a little small. Use ISBN number to fetch bigger one?
+        $("#Content").append('<p>'+Title +'</p>'+'<p>'+IsFiction+'</p>'+'<p> Brief description:<br>'+Description+'<p>')
+        if (Rating!=undefined){
+        $("#Content").append('<p> Average Rating: '+Rating+' stars</p>')
+        }
+        })
     
-    // run a for loop for 3 to get the first 3 searchs add an id of 0 1 or 2 for selecting search results?
-    var ISBN13=response.items[0].volumeInfo.industryIdentifiers[0].identifier;
-    var ISBN10=response.items[0].volumeInfo.industryIdentifiers[1].identifier;
-    var Author=response.items[0].volumeInfo.authors[0]
-    var Title=response.items[0].volumeInfo.title
-    var Image=response.items[0].volumeInfo.imageLinks.thumbnail
-    // next to it in the array are bigger images for the full book page
-    // In case we need either ISBN
-    console.log ("ISBN 13 and 10")
-    console.log (ISBN13)  
-    console.log (ISBN10)
-    console.log(Author)
-    console.log(response.items[0].volumeInfo.imageLinks.thumbnail)
-   console.log(Title)
-
 }
+    
+
 // These change the button to indicate we're searching by something
 // We'll add an ID to it to specify which function/api we'll be using for searching and giving back data
 $("#BAuthor").on("click", function(){
@@ -121,9 +172,11 @@ $("#MArtist").on("click", function(){
 function RemoveClass(){
     for (var i=0; i<SearchableTopics.length;i++){
         $("#Click").removeClass(SearchableTopics[i]);
+        $("#myDropdown").removeClass("show");
     }
     
 }
+
 })
 
 
